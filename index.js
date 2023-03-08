f51a4f318b057b80c536dbadcde5c3ae383f7cbe
moncheis <127621840+moncheis@users.noreply.github.com>
2023-03-08 17:59:01 -0300

    First iteration using playwright

--- index.js
+++ index.js
@@ -1,26 +1,33 @@
 const { program } = require('commander');
-const cheerio = require('cheerio');
-const bent = require('bent');
+const { chromium, devices } = require('playwright');
 
 program
   .argument("<url>")
-  .argument("<selectors...>")
-  .action(async (url, selectors) => {
-    const contents = await bent("string")(url); // get the page at the given URL
-    const $ = cheerio.load(contents); // process it
+  .argument("<locators...>")
+  .action(async (url, locators) => {
+    // Playwright setup
+    const browser = await chromium.launch();
+    const context = await browser.newContext(devices['Desktop Chrome']);
+    const page = await context.newPage();
+
+    // Don't load images.
+    await context.route('**/*.{png,jpg,jpeg}', route => route.abort());
+    await page.goto(url);
 
     // Use the tab space character for separating columns.
     const separator = "\t";
-    console.log("Selector" + separator + "Text") // header row
-    for (const selector of selectors) {
-      const text = $(selector).first().text();
-      console.log(selector + separator + text);
+    console.log("Locator" + separator + "Text") // header row
+    for (const locator of locators) {
+      const text = await page.locator(locator).first().innerText();
+      console.log(locator + separator + text);
     }
+
+    await browser.close();
   });
 
 program.addHelpText('before', `\
 For the page at the given URL, fetch the first HTML element that match each
-given selector and return their text values in CSV format.`);
+given Playwright locator and return their text values in CSV format.`);
 
 program.addHelpText('after', `\
 
